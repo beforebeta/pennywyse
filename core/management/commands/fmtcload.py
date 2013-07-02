56743b413d21cd2a8437a0ebee7744be0a415567
Supreme Overlord <beforebeta@googlemail.com>
2013-07-02 12:25:42 -0400

    updates

--- core/management/commands/fmtcload.py
+++ core/management/commands/fmtcload.py
@@ -3,7 +3,7 @@ from BeautifulSoup import BeautifulStoneSoup
 from django.core.management.base import BaseCommand
 from django.conf import settings
 import requests
-from core.models import DealType
+from core.models import DealType, Category
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
@@ -40,11 +40,13 @@ def refresh_categories():
     section("Loading Categories")
     data = requests.get("http://services.formetocoupon.com/getCategories?key=%s" % settings.FMTC_ACCESS_KEY)
     data = BeautifulStoneSoup(data.content)
-    for type in data.findAll("type"):
-        code = type.filter.text
-        name = type.find("name").text
-        DealType.objects.filter(code=code)
-        DealType(code=code,name=name,description=name).save()
+    for cat in data.findAll("category"):
+        ref_id = cat.find("id").text
+        code = cat.filter.text
+        name = cat.find("name").text
+        parent = cat.find("parent").text
+        Category.objects.filter(code=code).delete()
+        Category(ref_id=ref_id,code=code,name=name,parent=Category.objects.get(code=parent)).save()
         print "\t%s,%s" % (code,name)
 
 def load():
