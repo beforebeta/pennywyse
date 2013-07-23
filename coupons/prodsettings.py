be538cf80af533ba2a42d5373257239b87bd0f31
Supreme Overlord <beforebeta@googlemail.com>
2013-07-23 00:50:29 -0400

    updates

--- coupons/prodsettings.py
+++ coupons/prodsettings.py
@@ -1,6 +1,16 @@
 from basesettings import *
 
+base_dir = "/root/public_html/pennywyse/"
+
 SVCS_HOST = "http://www.pennywyse.com"
 
-MEDIA_ROOT = '/root/public_html/pennywyse/static/'
+MEDIA_ROOT = '%sstatic/'% base_dir
 MEDIA_URL = '/static/'
+
+TEMPLATE_DIRS = (
+    '%stemplates/' % base_dir,
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    )
+
