8953855964f0d09e5c9eff06af8605d071fe50ad
Supreme Overlord <beforebeta@googlemail.com>
2013-07-26 16:38:27 -0400

    fb og tags

--- web/admin.py
+++ web/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from web.models import ShortenedURLComponent
+from web.models import ShortenedURLComponent, NewCoupon, PopularCoupon, FeaturedCoupon
 
 admin.site.register(ShortenedURLComponent)
+admin.site.register(FeaturedCoupon)
+admin.site.register(NewCoupon)
+admin.site.register(PopularCoupon)
