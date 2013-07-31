1e098d5cce913186dbc2995a6e56a0f4275bd9e7
Supreme Overlord <beforebeta@googlemail.com>
2013-07-31 12:50:23 -0400

    session expiry extension

--- web/middleware.py
+++ web/middleware.py
@@ -2,4 +2,4 @@ from django.conf import settings
 
 class WebMiddleware(object):
     def process_request(self, request):
-        request.session.set_expiry(365 * 24 * 60 * 60) #one year
+        request.session.set_expiry(10 * 365 * 24 * 60 * 60) #ten years
