c443baf17f281c7638d6fef0b8220d9ca2f5b6bb
Steve Ellis <pair+steve@steveell.is>
2013-08-06 14:42:31 -0400

    Actually, always keep track of migrations

---
+++ web/migrations/0001_initial.py
@@ -0,0 +1,164 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'FeaturedCoupon'
+        db.create_table(u'web_featuredcoupon', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('coupon', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Coupon'])),
+            ('date_added', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2013, 8, 6, 0, 0), auto_now_add=True, blank=True)),
+            ('last_modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'web', ['FeaturedCoupon'])
+
+        # Adding model 'NewCoupon'
+        db.create_table(u'web_newcoupon', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('coupon', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Coupon'])),
+            ('date_added', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2013, 8, 6, 0, 0), auto_now_add=True, blank=True)),
+            ('last_modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'web', ['NewCoupon'])
+
+        # Adding model 'PopularCoupon'
+        db.create_table(u'web_popularcoupon', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('coupon', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Coupon'])),
+            ('date_added', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2013, 8, 6, 0, 0), auto_now_add=True, blank=True)),
+            ('last_modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'web', ['PopularCoupon'])
+
+        # Adding model 'ShortenedURLComponent'
+        db.create_table(u'web_shortenedurlcomponent', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('original_url', self.gf('django.db.models.fields.TextField')()),
+            ('shortened_url', self.gf('django.db.models.fields.CharField')(max_length=35, db_index=True)),
+        ))
+        db.send_create_signal(u'web', ['ShortenedURLComponent'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'FeaturedCoupon'
+        db.delete_table(u'web_featuredcoupon')
+
+        # Deleting model 'NewCoupon'
+        db.delete_table(u'web_newcoupon')
+
+        # Deleting model 'PopularCoupon'
+        db.delete_table(u'web_popularcoupon')
+
+        # Deleting model 'ShortenedURLComponent'
+        db.delete_table(u'web_shortenedurlcomponent')
+
+
+    models = {
+        u'core.category': {
+            'Meta': {'object_name': 'Category'},
+            'code': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 8, 6, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 8, 6, 0, 0)', 'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Category']", 'null': 'True', 'blank': 'True'}),
+            'ref_id': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        u'core.country': {
+            'Meta': {'object_name': 'Country'},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 8, 6, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 8, 6, 0, 0)', 'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'})
+        },
+        u'core.coupon': {
+            'Meta': {'object_name': 'Coupon'},
+            'categories': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['core.Category']", 'null': 'True', 'blank': 'True'}),
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'countries': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['core.Country']", 'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 8, 6, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'dealtypes': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['core.DealType']", 'null': 'True', 'blank': 'True'}),
+            'desc_slug': ('django.db.models.fields.CharField', [], {'default': "'COUPON'", 'max_length': '175'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'directlink': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'discount': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'end': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 8, 6, 0, 0)', 'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'lastupdated': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'link': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'listprice': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'merchant': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Merchant']", 'null': 'True', 'blank': 'True'}),
+            'percent': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'price': ('django.db.models.fields.FloatField', [], {'default': '0'}),
+            'ref_id': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'restrictions': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'short_desc': ('django.db.models.fields.CharField', [], {'default': "'COUPON'", 'max_length': '50'}),
+            'skimlinks': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'start': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        u'core.dealtype': {
+            'Meta': {'object_name': 'DealType'},
+            'code': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 8, 6, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 8, 6, 0, 0)', 'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        u'core.merchant': {
+            'Meta': {'object_name': 'Merchant'},
+            'coupon_count': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 8, 6, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'directlink': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 8, 6, 0, 0)', 'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'link': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'name_slug': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'ref_id': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'skimlinks': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'web.featuredcoupon': {
+            'Meta': {'object_name': 'FeaturedCoupon'},
+            'coupon': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Coupon']"}),
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 8, 6, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'})
+        },
+        u'web.newcoupon': {
+            'Meta': {'object_name': 'NewCoupon'},
+            'coupon': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Coupon']"}),
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 8, 6, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'})
+        },
+        u'web.popularcoupon': {
+            'Meta': {'object_name': 'PopularCoupon'},
+            'coupon': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Coupon']"}),
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 8, 6, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'})
+        },
+        u'web.shortenedurlcomponent': {
+            'Meta': {'object_name': 'ShortenedURLComponent'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'original_url': ('django.db.models.fields.TextField', [], {}),
+            'shortened_url': ('django.db.models.fields.CharField', [], {'max_length': '35', 'db_index': 'True'})
+        }
+    }
+
+    complete_apps = ['web']
---
+++ websvcs/migrations/0001_initial.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'ImageStore'
+        db.create_table(u'websvcs_imagestore', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('remote_url', self.gf('django.db.models.fields.CharField')(max_length=255, db_index=True)),
+            ('local_url', self.gf('django.db.models.fields.CharField')(max_length=255, db_index=True)),
+            ('source_user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], blank=True)),
+            ('width', self.gf('django.db.models.fields.IntegerField')(default=-1)),
+            ('height', self.gf('django.db.models.fields.IntegerField')(default=-1)),
+            ('date_added', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2013, 8, 6, 0, 0), auto_now_add=True, blank=True)),
+            ('last_modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'websvcs', ['ImageStore'])
+
+        # Adding model 'ShortenedURL'
+        db.create_table(u'websvcs_shortenedurl', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('original_url', self.gf('django.db.models.fields.TextField')()),
+            ('shortened_url', self.gf('django.db.models.fields.CharField')(max_length=40, db_index=True)),
+        ))
+        db.send_create_signal(u'websvcs', ['ShortenedURL'])
+
+        # Adding model 'EmailSubscription'
+        db.create_table(u'websvcs_emailsubscription', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('app', self.gf('django.db.models.fields.CharField')(max_length=255, db_index=True)),
+            ('session_key', self.gf('django.db.models.fields.CharField')(max_length=255, db_index=True)),
+            ('email', self.gf('django.db.models.fields.CharField')(max_length=255, db_index=True)),
+            ('first_name', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('full_name', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('context', self.gf('picklefield.fields.PickledObjectField')(default={})),
+            ('date_added', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2013, 8, 6, 0, 0), auto_now_add=True, blank=True)),
+            ('last_modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'websvcs', ['EmailSubscription'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'ImageStore'
+        db.delete_table(u'websvcs_imagestore')
+
+        # Deleting model 'ShortenedURL'
+        db.delete_table(u'websvcs_shortenedurl')
+
+        # Deleting model 'EmailSubscription'
+        db.delete_table(u'websvcs_emailsubscription')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'websvcs.emailsubscription': {
+            'Meta': {'object_name': 'EmailSubscription'},
+            'app': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
+            'context': ('picklefield.fields.PickledObjectField', [], {'default': '{}'}),
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 8, 6, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'full_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'session_key': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'})
+        },
+        u'websvcs.imagestore': {
+            'Meta': {'object_name': 'ImageStore'},
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 8, 6, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'height': ('django.db.models.fields.IntegerField', [], {'default': '-1'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'local_url': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
+            'remote_url': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
+            'source_user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']", 'blank': 'True'}),
+            'width': ('django.db.models.fields.IntegerField', [], {'default': '-1'})
+        },
+        u'websvcs.shortenedurl': {
+            'Meta': {'object_name': 'ShortenedURL'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'original_url': ('django.db.models.fields.TextField', [], {}),
+            'shortened_url': ('django.db.models.fields.CharField', [], {'max_length': '40', 'db_index': 'True'})
+        }
+    }
+
+    complete_apps = ['websvcs']
