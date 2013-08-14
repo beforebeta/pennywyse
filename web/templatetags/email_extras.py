76227e592dfe19efbe856f6530170588eec7a7ef
Steve Ellis <pair+steve@steveell.is>
2013-08-14 12:02:52 -0400

    Description is shown based on params

--- web/templatetags/email_extras.py
+++ web/templatetags/email_extras.py
@@ -3,5 +3,5 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('email/coupon.html')
-def show_coupon(coupon):
-    return {'coupon': coupon[0], 'index': coupon[1], 'coupon_url': coupon[2], 'merchant_url': coupon[3]}
+def show_coupon(coupon, store_description):
+    return {'coupon': coupon[0], 'index': coupon[1], 'coupon_url': coupon[2], 'merchant_url': coupon[3], 'store_description': store_description}
--- web/views/email.py
+++ web/views/email.py
@@ -8,7 +8,6 @@ def date_string():
     date_string = datetime.datetime.now().strftime("%d, %b %Y")
 
 def coupon_tuple(link_type):
-    all_coupons = Coupon.objects.all()
     coupon_data = []
     index = 0
     for slug in merchant_slugs(None):
@@ -16,7 +15,6 @@ def coupon_tuple(link_type):
         merchant = Merchant.objects.get(name_slug=slug)
         coupon = merchant.featured_coupon()
         merchant_link = "http://pennywyse.com/coupons/{0}/{1}".format(merchant.name_slug, merchant.id)
-        link_address = None
         if link_type == 'merchant':
           link_address = merchant_link
         else:
@@ -27,34 +25,46 @@ def coupon_tuple(link_type):
         coupon_data.append([coupon, index, link_address, merchant_link])
     return coupon_data
 
-def email_a(self):
+def store_description(request):
+    desc = request.GET.get('store_description', True)
+    if (desc == 'False') | (desc == 'false'):
+        return False
+    else:
+        return True
+
+
+def email_a(request):
     context = {
-        'coupon_data' : coupon_tuple('coupon'),
+        'coupon_data': coupon_tuple('coupon'),
         'date_string': date_string(),
         'show_image': True,
+        'store_description': store_description(request)
     }
     return render_to_response("email.html", context)
 
-def email_b(self):
+def email_b(request):
     context = {
-        'coupon_data' : coupon_tuple('merchant'),
+        'coupon_data': coupon_tuple('merchant'),
         'date_string': date_string(),
-        'show_image': True
+        'show_image': True,
+        'store_description': store_description(request)
     }
     return render_to_response("email.html", context)
 
-def email_c(self):
+def email_c(request):
     context = {
-        'coupon_data' : coupon_tuple('coupon'),
+        'coupon_data': coupon_tuple('coupon'),
         'date_string': date_string(),
-        'show_image': False
+        'show_image': False,
+        'store_description': store_description(request)
     }
     return render_to_response("email.html", context)
 
-def email_d(self):
+def email_d(request):
     context = {
-        'coupon_data' : coupon_tuple('merchant'),
+        'coupon_data': coupon_tuple('merchant'),
         'date_string': date_string(),
-        'show_image': False
+        'show_image': False,
+        'store_description': store_description(request)
     }
     return render_to_response("email.html", context)
