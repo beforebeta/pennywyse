ea811d9802ff5e0253f8d0fd497aa4dbdfd88c68
Amrish Singh <beforebeta@gmail.com>
2013-08-14 11:10:25 -0400

    removed bug that direct open of /coupon/ page doesnt get click tracked - TRY 3

--- tracking/views.py
+++ tracking/views.py
@@ -24,7 +24,7 @@ def _remove_skimlinks(skimlinked_url):
 
 def log_click_track(request):
     try:
-        print request.PATH
+        print request.path
         click_track(request)
     except:
         print_stack_trace()
