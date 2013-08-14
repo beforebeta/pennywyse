4c4f50811a2ae1730746a7eadc8224686571c616
Steve Ellis <pair+steve@steveell.is>
2013-08-14 11:20:35 -0400

    Dry up the email views

--- web/views/email.py
+++ web/views/email.py
@@ -4,72 +4,57 @@ from django.http import HttpResponseRedirect
 from core.models import Coupon, Merchant
 from tracking.commission.skimlinks import merchant_slugs
 
-def coupon_tuple(slug, index, base_link, merchant_link):
-      merchant = Merchant.objects.filter(name_slug=slug)[0]
-      coupon = merchant.featured_coupon()
-      params = "utm_medium=mailchimp&utm_source=newsletter&utm_campaign=promo&utm_term={0}".format(merchant.name_slug)
-      link = "{0}?{1}".format(base_link, params)
-      more_merchant_link = "{0}?{1}".format(merchant_link, params)
-      return [coupon, index+1, link, more_merchant_link]
+def date_string():
+    date_string = datetime.datetime.now().strftime("%d, %b %Y")
 
-def email_a(self):
+def coupon_tuple(link_type):
     all_coupons = Coupon.objects.all()
     coupon_data = []
-    i = 0
-    date_string = datetime.datetime.now().strftime("%d, %b %Y")
+    index = 0
     for slug in merchant_slugs(None):
-        merchant = Merchant.objects.filter(name_slug=slug)[0]
+        index += 1
+        merchant = Merchant.objects.get(name_slug=slug)
         coupon = merchant.featured_coupon()
         merchant_link = "http://pennywyse.com/coupons/{0}/{1}".format(merchant.name_slug, merchant.id)
-        coupon_data.append(coupon_tuple(slug, i, merchant_link, merchant_link))
-        i += 1
+        link_address = None
+        if link_type == 'merchant':
+          link_address = merchant_link
+        else:
+          link_address = "http://pennywyse.com/coupon/{0}/{1}/{2}".format(merchant.name_slug, coupon.desc_slug, coupon.id)
+        params = "utm_medium=mailchimp&utm_source=newsletter&utm_campaign=promo&utm_term={0}".format(merchant.name_slug)
+        link_address = "{0}?{1}".format(link_address, params)
+        merchant_link = "{0}?{1}".format(merchant_link, params)
+        coupon_data.append([coupon, index, link_address, merchant_link])
+    return coupon_data
 
-    context = {'coupon_data' : coupon_data, 'date_string': date_string, 'show_image': True}
+def email_a(self):
+    context = {
+        'coupon_data' : coupon_tuple('coupon'),
+        'date_string': date_string(),
+        'show_image': True,
+    }
     return render_to_response("email.html", context)
 
 def email_b(self):
-    all_coupons = Coupon.objects.all()
-    coupon_data = []
-    i = 0
-    date_string = datetime.datetime.now().strftime("%d, %b %Y")
-    for slug in merchant_slugs(None):
-        merchant = Merchant.objects.filter(name_slug=slug)[0]
-        coupon = merchant.featured_coupon()
-        merchant_link = "http://pennywyse.com/coupons/{0}/{1}".format(merchant.name_slug, merchant.id)
-        coupon_link = "http://pennywyse.com/coupon/{0}/{1}/{2}".format(merchant.name_slug, coupon.desc_slug, coupon.id)
-        coupon_data.append(coupon_tuple(slug, i, coupon_link, merchant_link))
-        i += 1
-
-    context = {'coupon_data' : coupon_data, 'date_string': date_string, 'show_image': True}
+    context = {
+        'coupon_data' : coupon_tuple('merchant'),
+        'date_string': date_string(),
+        'show_image': True
+    }
     return render_to_response("email.html", context)
 
 def email_c(self):
-    all_coupons = Coupon.objects.all()
-    coupon_data = []
-    i = 0
-    date_string = datetime.datetime.now().strftime("%d, %b %Y")
-    for slug in merchant_slugs(None):
-        merchant = Merchant.objects.filter(name_slug=slug)[0]
-        coupon = merchant.featured_coupon()
-        merchant_link = "http://pennywyse.com/coupons/{0}/{1}".format(merchant.name_slug, merchant.id)
-        coupon_data.append(coupon_tuple(slug, i, merchant_link, merchant_link))
-        i += 1
-
-    context = {'coupon_data' : coupon_data, 'date_string': date_string, 'show_image': False}
+    context = {
+        'coupon_data' : coupon_tuple('coupon'),
+        'date_string': date_string(),
+        'show_image': False
+    }
     return render_to_response("email.html", context)
 
 def email_d(self):
-    all_coupons = Coupon.objects.all()
-    coupon_data = []
-    i = 0
-    date_string = datetime.datetime.now().strftime("%d, %b %Y")
-    for slug in merchant_slugs(None):
-        merchant = Merchant.objects.filter(name_slug=slug)[0]
-        coupon = merchant.featured_coupon()
-        merchant_link = "http://pennywyse.com/coupons/{0}/{1}".format(merchant.name_slug, merchant.id)
-        coupon_link = "http://pennywyse.com/coupon/{0}/{1}/{2}".format(merchant.name_slug, coupon.desc_slug, coupon.id)
-        coupon_data.append(coupon_tuple(slug, i, coupon_link, merchant_link))
-        i += 1
-
-    context = {'coupon_data' : coupon_data, 'date_string': date_string, 'show_image': False}
+    context = {
+        'coupon_data' : coupon_tuple('merchant'),
+        'date_string': date_string(),
+        'show_image': False
+    }
     return render_to_response("email.html", context)
