9a75a7d3160036ad57ddafeadd87b8bf16c405b2
Steve Ellis <email@steveell.is>
2013-08-22 11:59:40 -0400

    Make sure datetime is included in all environments

--- coupons/basesettings.py
+++ coupons/basesettings.py
@@ -1,4 +1,4 @@
-import os
+import os, datetime
 
 __author__ = 'amrish'
 
--- coupons/settings.py
+++ coupons/settings.py
@@ -1,5 +1,4 @@
 # Django settings for coupons project.
-import os, datetime
 from basesettings import *
 
 SVCS_HOST = "http://127.0.0.1:8000"
