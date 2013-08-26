d4a34c9c62f2ac6dac12492cf8b75a024121ca4b
Amrish Singh <beforebeta@gmail.com>
2013-08-26 15:56:39 -0400

    Improved Visitor Admin

--- tracking/admin.py
+++ tracking/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from tracking.models import BannedIP, UntrackedUserAgent, Visitor, ClickTrack, RevenueVisitor
 
-
 class ClickTrackAdmin(admin.ModelAdmin):
     readonly_fields = ('coupon','merchant',)
     ordering = ['-date_added']
 
+class VisitorAdmin(admin.ModelAdmin):
+    fields = ('url', 'acquisition_source', 'acquisition_medium','acquisition_campaign','acquisition_term','acquisition_content')
+
 admin.site.register(BannedIP)
 admin.site.register(UntrackedUserAgent)
-admin.site.register(Visitor)
+admin.site.register(Visitor, VisitorAdmin)
 admin.site.register(RevenueVisitor)
 admin.site.register(ClickTrack, ClickTrackAdmin)
