37452aa1b82eb3e04a04d48b48bd2a204af42a49
Steve Ellis <email@steveell.is>
2013-09-03 15:31:58 -0400

    Make s3 backups private.

--- core/management/commands/backup_db.py
+++ core/management/commands/backup_db.py
@@ -1,17 +1,21 @@
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 from django.core.management.base import BaseCommand
+from django.conf import settings
 from subprocess import check_output
 import datetime
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        file_name = 'coupons{0}.sql'.format(datetime.datetime.now().strftime('%Y%m%d%H%M'))
+        settings.AWS_DEFAULT_ACL = 'private'
+
+        file_name = 'backups/coupons{0}.sql'.format(datetime.datetime.now().strftime('%Y%m%d%H%M'))
 
         self.stdout.write('Grabbing database dump...\n\n')
         sql_string = check_output(['mysqldump', '-u', 'dbuser', '-p', '--databases', 'coupons', 'information_schema', '--skip-lock-tables'])
 
         self.stdout.write('Uploading to S3...\n\n')
-        default_storage.save('backups/{0}'.format(file_name), ContentFile(sql_string))
+        default_storage.save(file_name, ContentFile(sql_string))
+
 
         self.stdout.write('Wrapping up...\n\n')
