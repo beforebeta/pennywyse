6f8aaa34696aa568eb2f35ac075673180cd22a8f
Alexander Gaevsky <sasha@sasha0.ru>
2013-10-04 18:46:20 +0300

    refactored check sitemap functionality for concurrent URL check, added mandrill stmp gateway for sending reports, #7964

--- core/management/commands/check_sitemap.py
+++ core/management/commands/check_sitemap.py
@@ -1,41 +1,64 @@
+import sys
 from bs4 import BeautifulSoup
 from django.conf import settings
 from django.core.mail import EmailMessage
 from django.core.management.base import BaseCommand
 import requests
+import grequests
 
 SITEMAP_URL = 'http://s3.amazonaws.com/pennywyse/sitemap.xml'
-TO_EMAIL = 'amrish@pennywyse.com'
 
 class Command(BaseCommand):
+    urls = []
+    error_urls = []
+    checked_urls = 0
 
-    def _check_urls(self, url):
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text)
-        for location in soup.findAll('loc'):
-            if location.text.endswith('.xml'):
-                self.stdout.write('Checking sitemap %s' % location.text)
-                self._check_urls(location.text)
-            else:
-                self._check_url(location.text)
+    def _check_sitemap(self, url):
+        """
+        Fetching and parsing main sitemap, extracting sitemaps locations,
+        then parsing them and preparing URLs for check.
+        """
+        
+        try:
+            r = requests.get(url)
+            soup = BeautifulSoup(r.text)
+            for location in soup.findAll('loc'):
+                if location.text.endswith('.xml'):
+                    self._check_sitemap(location.text)
+                else:
+                    self.urls.append(location.text)
+                    # when number of URLs has reached SITEMAP_CHECK_LIMIT, making concurrent requests for those URLs
+                    if len(self.urls) == settings.SITEMAP_CHECK_LIMIT:
+                        self._check_urls()
+            self._check_urls()
+                    
+        except Exception as e:
+            self.stdout.write('Error: %s' % str(e))
 
-    def _check_url(self, url):
-        r = requests.head(url)
-        if r.status_code in [200, 301, 302]:
-            self.stdout.write('%s OK' % url)
-        else:
-            self.stdout.write('%s ERROR' % url)
-            return url
-
+    def _check_urls(self):
+        """
+        Concurrently checking bunch of URLs, limited by SITEMAP_CHECK_LIMIT setting,
+        displaying progress in console.
+        """
+        
+        r = (grequests.head(u) for u in self.urls)
+        rs = grequests.map(r)
+        self.error_urls += filter(lambda x: x.status_code not in [200, 301, 302], rs)
+        self.checked_urls += len(self.urls)
+        sys.stdout.write('\rChecked %s URLs, %s errors.' % (self.checked_urls, len(self.error_urls)))
+        sys.stdout.flush()
+        self.urls = []
 
     def handle(self, *args, **options):
-        self.stdout.write('Checking sitemaps.')
-        error_urls = [u for u in self._check_urls(SITEMAP_URL)]
-        if error_urls:
+        sys.stdout.write('\rChecking sitemaps.')
+        self._check_sitemap(SITEMAP_URL)
+        # if there any URLs that hasn't passed check - sending report by email to addresses, listed in SITEMAP_REPORT_RECIPIENTS
+        if self.error_urls:
             report = '\n'.join(error_urls)
             email_message = EmailMessage('Sitemaps report', body='Report with list of URLs which returned error response codes is attached.', 
-                                         from_email=settings.DEFAULT_FROM_EMAIL, to=[TO_EMAIL])
+                                         from_email=settings.DEFAULT_FROM_EMAIL, to=settings.SITEMAP_REPORT_RECIPIENTS)
             email_message.attach('report.txt', report,'text/plain')
             email_message.send()
         else:
-            self.stdout.write('All pages returned success response codes.')
+            sys.stdout.flush()
+            sys.stdout.write('\rAll pages returned success response codes.')
