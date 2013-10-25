91c776df6c1ce9a363c3f41ea125cdab7b3d857a
Alexander Kulakov <a.kulakov@mail.ru>
2013-10-25 19:04:33 -0400

    Improved backup_db command
    Previous backup_db command was loading whole database backup into memory
    and was failing because of memory errors

--- core/management/commands/backup_db.py
+++ core/management/commands/backup_db.py
@@ -1,21 +1,33 @@
-from django.core.files.base import ContentFile
+from django.core.files.base import File
 from django.core.files.storage import default_storage
 from django.core.management.base import BaseCommand
 from django.conf import settings
-from subprocess import check_output
+from subprocess import check_call
 import datetime
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         settings.AWS_DEFAULT_ACL = 'private'
+        settings.AWS_REDUCED_REDUNDANCY = False
 
-        file_name = 'backups/coupons{0}.sql'.format(datetime.datetime.now().strftime('%Y%m%d%H%M'))
+        db = settings.DATABASES['default']
+        database = db['NAME']
+        user = db['USER']
+        password = db['PASSWORD']
 
-        self.stdout.write('Grabbing database dump...\n\n')
-        sql_string = check_output(['mysqldump', '-u', 'dbuser', '--password=dbuser', 'coupons'])
+        file_name = 'backups/{0}{1}.sql'.format(database,
+                                                 datetime.datetime.now()
+                                                 .strftime('%Y%m%d%H%M'))
 
-        self.stdout.write('Uploading to S3...\n\n')
-        default_storage.save(file_name, ContentFile(sql_string))
+        with open(file_name, 'wb') as backup:
+            self.stdout.write('Grabbing database dump...\n\n')
+            check_call(['mysqldump',
+                        '--user=%s' % user,
+                        '--password=%s' % password,
+                        database], stdout=backup)
 
+        with open(file_name) as backup:
+            self.stdout.write('Uploading to S3...\n\n')
+            default_storage.save(file_name, File(backup))
 
         self.stdout.write('Wrapping up...\n\n')
