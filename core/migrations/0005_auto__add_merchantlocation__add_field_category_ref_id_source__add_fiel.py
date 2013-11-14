b1ef8f4dba61c381252c23a88e35196c31b5099b
Alexander Gaevsky <sasha@sasha0.ru>
2013-11-15 00:34:35 +0200

    merged migrations for sqoot integration, #8093

--- core/migrations/0005_auto__add_merchantlocation__add_field_category_ref_id_source__add_fiel.py
+++ core/migrations/0005_auto__add_merchantlocation__add_field_category_ref_id_source__add_fiel.py
@@ -8,12 +8,34 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Deleting field 'MerchantLocation.coupon'
-        db.delete_column(u'core_merchantlocation', 'coupon_id')
+        # Adding model 'MerchantLocation'
+        db.create_table(u'core_merchantlocation', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('merchant', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Merchant'], null=True, blank=True)),
+            ('geometry', self.gf('django.contrib.gis.db.models.fields.PointField')()),
+            ('address', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('locality', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('region', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('postal_code', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('country', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('date_added', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2013, 11, 14, 0, 0), auto_now_add=True, blank=True)),
+            ('last_modified', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2013, 11, 14, 0, 0), auto_now=True, auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'core', ['MerchantLocation'])
 
-        # Adding field 'MerchantLocation.merchant'
-        db.add_column(u'core_merchantlocation', 'merchant',
-                      self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Merchant'], null=True, blank=True),
+        # Adding field 'Category.ref_id_source'
+        db.add_column(u'core_category', 'ref_id_source',
+                      self.gf('django.db.models.fields.CharField')(db_index=True, max_length=255, null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Coupon.ref_id_source'
+        db.add_column(u'core_coupon', 'ref_id_source',
+                      self.gf('django.db.models.fields.CharField')(db_index=True, max_length=255, null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Coupon.online'
+        db.add_column(u'core_coupon', 'online',
+                      self.gf('django.db.models.fields.NullBooleanField')(null=True, blank=True),
                       keep_default=False)
 
         # Adding field 'Coupon.merchant_location'
@@ -21,29 +43,57 @@ class Migration(SchemaMigration):
                       self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.MerchantLocation'], null=True, blank=True),
                       keep_default=False)
 
-
-    def backwards(self, orm):
-        # Adding field 'MerchantLocation.coupon'
-        db.add_column(u'core_merchantlocation', 'coupon',
-                      self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Coupon'], null=True, blank=True),
+        # Adding field 'Coupon.coupon_network'
+        db.add_column(u'core_coupon', 'coupon_network',
+                      self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.CouponNetwork'], null=True, blank=True),
                       keep_default=False)
 
-        # Deleting field 'MerchantLocation.merchant'
-        db.delete_column(u'core_merchantlocation', 'merchant_id')
+        # Adding field 'Merchant.ref_id_source'
+        db.add_column(u'core_merchant', 'ref_id_source',
+                      self.gf('django.db.models.fields.CharField')(db_index=True, max_length=255, null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'CouponNetwork.code'
+        db.add_column(u'core_couponnetwork', 'code',
+                      self.gf('django.db.models.fields.CharField')(db_index=True, max_length=255, null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting model 'MerchantLocation'
+        db.delete_table(u'core_merchantlocation')
+
+        # Deleting field 'Category.ref_id_source'
+        db.delete_column(u'core_category', 'ref_id_source')
+
+        # Deleting field 'Coupon.ref_id_source'
+        db.delete_column(u'core_coupon', 'ref_id_source')
+
+        # Deleting field 'Coupon.online'
+        db.delete_column(u'core_coupon', 'online')
 
         # Deleting field 'Coupon.merchant_location'
         db.delete_column(u'core_coupon', 'merchant_location_id')
+
+        # Deleting field 'Coupon.coupon_network'
+        db.delete_column(u'core_coupon', 'coupon_network_id')
+
+        # Deleting field 'Merchant.ref_id_source'
+        db.delete_column(u'core_merchant', 'ref_id_source')
+
+        # Deleting field 'CouponNetwork.code'
+        db.delete_column(u'core_couponnetwork', 'code')
 
 
     models = {
         u'core.category': {
             'Meta': {'object_name': 'Category'},
             'code': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 11, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 14, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
             'description': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'image': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'last_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 11, 0, 0)', 'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 14, 0, 0)', 'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'}),
             'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Category']", 'null': 'True', 'blank': 'True'}),
             'ref_id': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'}),
@@ -52,9 +102,9 @@ class Migration(SchemaMigration):
         u'core.country': {
             'Meta': {'object_name': 'Country'},
             'code': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
-            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 11, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 14, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'last_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 11, 0, 0)', 'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 14, 0, 0)', 'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'})
         },
         u'core.coupon': {
@@ -64,7 +114,7 @@ class Migration(SchemaMigration):
             'countries': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['core.Country']", 'null': 'True', 'blank': 'True'}),
             'coupon_network': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.CouponNetwork']", 'null': 'True', 'blank': 'True'}),
             'created': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
-            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 11, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 14, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
             'dealtypes': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['core.DealType']", 'null': 'True', 'blank': 'True'}),
             'desc_slug': ('django.db.models.fields.CharField', [], {'default': "u'COUPON'", 'max_length': '175'}),
             'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
@@ -76,7 +126,7 @@ class Migration(SchemaMigration):
             'end': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'image': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'last_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 11, 0, 0)', 'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 14, 0, 0)', 'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
             'lastupdated': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
             'link': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             'listprice': ('django.db.models.fields.FloatField', [], {'default': '0'}),
@@ -96,45 +146,56 @@ class Migration(SchemaMigration):
         u'core.couponnetwork': {
             'Meta': {'object_name': 'CouponNetwork'},
             'code': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 11, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 14, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'last_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 11, 0, 0)', 'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 14, 0, 0)', 'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'})
         },
         u'core.dealtype': {
             'Meta': {'object_name': 'DealType'},
             'code': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 11, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 14, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
             'description': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'last_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 11, 0, 0)', 'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 14, 0, 0)', 'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'})
         },
         u'core.merchant': {
             'Meta': {'object_name': 'Merchant'},
             'coupon_count': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
-            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 11, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 14, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
             'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             'directlink': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'image': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'last_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 11, 0, 0)', 'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 14, 0, 0)', 'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
             'link': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'}),
             'name_slug': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'}),
             'redirect': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
-            'ref_id': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'ref_id': ('django.db.models.fields.CharField', [], {'default': "u'refid'", 'max_length': '255', 'null': 'True', 'db_index': 'True', 'blank': 'True'}),
             'ref_id_source': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'null': 'True', 'blank': 'True'}),
             'skimlinks': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'core.merchantaffiliatedata': {
+            'Meta': {'object_name': 'MerchantAffiliateData'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'link': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'merchant': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'affiliate_data'", 'to': u"orm['core.Merchant']"}),
+            'network': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'networkid': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'networknote': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'primary': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'ref_id': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
         },
         u'core.merchantlocation': {
             'Meta': {'object_name': 'MerchantLocation'},
             'address': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
             'country': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 11, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 14, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
             'geometry': ('django.contrib.gis.db.models.fields.PointField', [], {}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'last_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 11, 0, 0)', 'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 11, 14, 0, 0)', 'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
             'locality': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
             'merchant': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['core.Merchant']", 'null': 'True', 'blank': 'True'}),
             'postal_code': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
