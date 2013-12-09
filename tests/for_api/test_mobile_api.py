aff391dbc6443fa9c72d3d089adae082cd3a8b0f
ThomasWhyyou <thomas.whyyou@ymail.com>
2013-12-09 18:44:51 -0500

    Wrote another test in test_mobile_api.py for the ordering of deals by distance

--- tests/for_api/test_mobile_api.py
+++ tests/for_api/test_mobile_api.py
@@ -1,6 +1,7 @@
 import requests
 import random
 from django.conf import settings
+from django.template.defaultfilters import slugify
 from .sample_data_feed import top_50_us_cities_dict, sample_query_strings
 
 request_parameters = {
@@ -15,25 +16,26 @@ SQOOT_API_URL = "http://api.sqoot.com/v2/"
 LOCAL_API_URL = "http://localhost:8000/v3/"
 MATCH_THRESHOLD = 0.6
 
-def test_local_deals_api_page_one_vs_sqoot():
-    # categories_list = get_categories_list()
-    # for category in categories_list:
-    request_parameters['query'] = 'spa'
-    for location in top_50_us_cities_dict.values():
-        request_parameters['location'] = location
-        # request_parameters['location'] = "40.75170,-73.99420"
-        local_response, sqoot_response = fetch_local_and_sqoot_responses(request_parameters)
-        sqoot_deals_list = sqoot_response['deals']
-        local_deals_list = local_response['deals']
-        if len(sqoot_deals_list) == 0:
-            continue
-        else:
-            yield check_deals_match_vs_threshold, local_deals_list, sqoot_deals_list
+# def test_local_deals_api_page_one_vs_sqoot():
+#     categories_list = get_categories_list()
+#     picked_category = random.sample(categories_list, 1)[0]
+#     request_parameters['category_slugs'] = slugify(picked_category)
+#     for location in top_50_us_cities_dict.values():
+#         request_parameters['location'] = location
+#         local_response, sqoot_response = fetch_local_and_sqoot_responses(request_parameters)
+#         sqoot_deals_list = sqoot_response['deals']
+#         local_deals_list = local_response['deals']
+#         if len(sqoot_deals_list) == 0:
+#             continue
+#         else:
+#             yield check_deals_match_vs_threshold, local_deals_list, sqoot_deals_list
 
 def test_local_deals_response_ordered_by_distance():
     picked_location = random.sample(top_50_us_cities_dict.values(), 1)
     request_parameters['location'] = picked_location
     local_response, sqoot_response = fetch_local_and_sqoot_responses(request_parameters)
+    distance_list = [int(deal['deal']['merchant']['dist_to_user_mi']) for deal in local_response['deals']]
+    assert distance_list == sorted(distance_list)
 
 
 ##################################################################################################
@@ -53,3 +55,8 @@ def fetch_local_and_sqoot_responses(request_parameters):
     local_response = requests.get(LOCAL_API_URL + 'deals', params=request_parameters).json()
     sqoot_response = requests.get(SQOOT_API_URL + 'deals', params=request_parameters).json()
     return local_response, sqoot_response
+
+def get_categories_list():
+    sqoot_categories = requests.get(SQOOT_API_URL + 'categories', params=request_parameters).json()['categories']
+    categories_list = [category['category']['name'] for category in sqoot_categories]
+    return categories_list
