74cdd0d05bd8b8b2d07328e31e0a56d82de04a26
ThomasWhyyou <thomas.whyyou@ymail.com>
2013-12-18 13:04:28 -0500

    Finished writing the first draft of new mobile_api.py

--- api/mobile_api.py
+++ api/mobile_api.py
@@ -1,21 +1,28 @@
+import string
+import random
+
 from tastypie.resources import ModelResource
 # from geopy import geocoders
 from geopy.distance import distance as geopy_distance
 from haystack.query import SearchQuerySet
 from haystack.utils.geo import Point, D
+from elasticsearch import Elasticsearch
 
-from core.models import Coupon
+from core.util import print_stack_trace
+from core.models import Coupon, Category
 
-class DealsResource(ModelResource):
-    class Meta:
-        # queryset = Coupon.all_objects.filter(ref_id_source='sqoot')
-        queryset = Coupon.objects.filter(ref_id_source='sqoot')
-        resource_name = 'deals'
+class MobileResource(ModelResource):
 
-    def return_response(self, request, **kwargs):
+    def __init__(self, *args, **kwargs):
+        # super(MobileResource, self).save(*args, **kwargs) # didn't work
+        super(MobileResource, self).__init__()
+        self.es = Elasticsearch()
+        self.available_categories_list = [c.name for c in Category.objects.all()]
+        self.sanitized_categories_list = [c for c in self.available_categories_list if self.check_if_should_exclude(c)]
+
+    def deals_return_response(self, request, **kwargs):
         self.method_check(request, allowed=['get'])
 
-        # g = geocoders.GoogleV3()
         params_dict = request.GET
         params_keys = params_dict.keys()
 
@@ -24,17 +31,20 @@ class DealsResource(ModelResource):
             lat_lng_in_list = location_param.split(',')
             lat = float(lat_lng_in_list[0])
             lng = float(lat_lng_in_list[1])
-            # location, (lat, lng) = g.geocode(location_param)
         except:
             response = {
                 'error': {'message': "You must supply a valid user location information."}
             }
             return self.create_response(request, response)
 
-        '''
-        # Check: sqs ordering based on proximity to user
-        # NEED UPDATED_AFTER
-        '''
+        try:
+            id_param = params_dict['id']
+        except:
+            response = {
+                'error': {'message': "You must supply a valid user uuid."}
+            }
+            return self.create_response(request, response)
+
         radius = D(mi=float(params_dict['radius'])) if 'radius' in params_keys else D(mi=10)
         user_pnt = Point(lng, lat)
         sqs = SearchQuerySet().filter(django_ct='core.coupon', coupon_source='sqoot', online=False, is_duplicate=False).dwithin('merchant_location', user_pnt, radius).distance('merchant_location', user_pnt).order_by('distance')
@@ -43,6 +53,12 @@ class DealsResource(ModelResource):
             query = params_dict['query']
             sqs_by_query = SearchQuerySet().filter(mobilequery=query)
             sqs = sqs.__and__(sqs_by_query)
+
+            # Prepare for 'localindex' api service
+            self.create_localinfo_index_if_doesnt_exist()
+            matched_category_indices = [i for i, s in enumerate(self.available_categories_list) if query.lower() in s.lower()]
+            matched_category_names = [self.available_categories_list[i] for i in matched_category_indices]
+            self.index_it_in_localinfo_populars(id_param, location_param, string.capwords(query), matched_category_names)
 
         if 'category_slugs' in params_keys:
             category_slugs_list = params_dict['category_slugs'].split(',')
@@ -147,3 +163,150 @@ class DealsResource(ModelResource):
         }
 
         return self.create_response(request, response)
+
+
+    def localinfo_return_response(self, request, **kwargs):
+        self.method_check(request, allowed=['get'])
+
+        params_dict = request.GET
+        try:
+            location_param = params_dict['location']
+        except:
+            response = {
+                'error': {'message': "You must supply a valid user location information."}
+            }
+            return self.create_response(request, response)
+
+        default_radius_filter = '5mi'
+        filters = {
+            "query": {
+                "filtered" : {
+                    "query" : {
+                        "match_all" : {}
+                    },
+                    "filter" : {
+                        "geo_distance" : {
+                            "distance" : default_radius_filter,
+                            "location" : location_param
+                        }
+                    }
+                }
+            },
+            "facets": {
+                "search_keyword" : {
+                    "terms" : {
+                        "field" : "search_keyword",
+                        "order" : "count"
+                    }
+                },
+                "search_category" : {
+                    "terms" : {
+                        "field" : "search_category",
+                        "order" : "count"
+                    }
+                }
+            }
+        }
+
+        res = self.es.search(index="localinfo", doc_type='populars', body=filters)
+        popular_category_list_raw = res['facets']['search_category']['terms']
+        popular_nearby_list_raw = res['facets']['search_keyword']['terms']
+
+        base_response = {
+            "default_image": "http://s3.amazonaws.com/pushpennyapp/default-placeholder.jpg",
+            "search_categories": []
+        }
+        popular_category_sub_structure = {
+            "name": "Popular Categories",
+            "list": []
+        }
+        popular_nearby_sub_structure = {
+            "name": "Popular Nearby",
+            "list": []
+        }
+
+        # Always show 'popular categories' based on either user search history or random suggestions.
+        max_categories = 6
+        for c in popular_category_list_raw[:max_categories]:
+            if c['count'] < 5:
+                break
+            else:
+                popular_category = self.return_popular_something_insert(c['term'])
+                popular_category_sub_structure['list'].append(popular_category)
+
+        while True:
+            popular_categories_so_far = [c['name'] for c in popular_category_sub_structure['list']]
+            if len(popular_categories_so_far) >= max_categories:
+                break
+            random_pick = random.sample(self.sanitized_categories_list, 1)[0]
+            if random_pick not in popular_categories_so_far:
+                 # popular_category_sub_structure['list']
+                 popular_category = self.return_popular_something_insert(random_pick)
+                 popular_category_sub_structure['list'].append(popular_category)
+        base_response['search_categories'].append(popular_category_sub_structure)
+
+        # Show 'popular nearby' based on user search history ONLY IF 100 or above history;
+        # 'nearby' and 'keyword' used interchangeably.
+        max_nearbys = 5
+        for kw in popular_nearby_list_raw[:max_nearbys]:
+            if kw['count'] < 5:
+                break
+            else:
+                popular_nearby = self.return_popular_something_insert(kw['term'])
+                popular_nearby_sub_structure['list'].append(popular_nearby)
+        if popular_nearby_sub_structure['list']:
+            base_response['search_categories'].append(popular_nearby_sub_structure)
+
+        # only for debugging purposes below
+        base_response['elasticsearch_res'] = res
+
+        response = base_response
+        return self.create_response(request, response)
+
+    ############################################################################################
+    # Helper Methods
+    ############################################################################################
+
+    def create_localinfo_index_if_doesnt_exist(self):
+        if not self.es.indices.exists(index='localinfo'):
+            try:
+                settings_and_mappings = {
+                    "mappings": {
+                        "populars": {
+                            "properties": {
+                                "user_uuid": {"type": "string"},
+                                "location": {"type": "geo_point"},
+                                "search_keyword": {"type": "string"},
+                                "search_category": {"type": "string"}
+                            }
+                        }
+                    }
+                }
+                self.es.indices.create(index='localinfo', body=settings_and_mappings)
+            except:
+                print_stack_trace()
+
+    def index_it_in_localinfo_populars(self, user_uuid, location, search_keyword, search_category):
+        localinfo_data_capture = {
+            'user_uuid': user_uuid,
+            'location': location,
+            'search_keyword': search_keyword,
+            'search_category': search_category if search_category else None
+        }
+        self.es.index(index="localinfo", doc_type='populars', body=localinfo_data_capture)
+
+    def check_if_should_exclude(self, category):
+        categories_to_exclude_list = ['Jewish', 'Gay', 'Special Interest']
+        if category in categories_to_exclude_list:
+            return False
+        else:
+            return True
+
+    def return_popular_something_insert(self, category_or_keyword):
+        popular_something = {
+            "image": None,
+            "name": category_or_keyword
+        }
+        return popular_something
+
+
