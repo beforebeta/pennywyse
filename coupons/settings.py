42a367d86d0bad65af562b17ce638ea71c79f0af
ThomasWhyyou <thomas.whyyou@ymail.com>
2013-12-26 10:09:46 -0500

    before rebasing with the latest upstream/master

--- coupons/settings.py
+++ coupons/settings.py
@@ -7,16 +7,15 @@ SVCS_HOST = "http://127.0.0.1:8000"
 # for debug purposes
 CELERY_ALWAYS_EAGER = True
 
-# HAYSTACK_CONNECTIONS = {
-#     'default': {
-#         'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
-#         'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
-#     },
-# }
-
 HAYSTACK_CONNECTIONS = {
     'default': {
-        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
-        'URL': 'http://127.0.0.1:8080/solr'
+        'ENGINE': 'web.utils.CustomSolrEngine',
+        'URL': 'http://127.0.0.1:8080/solr',
+        'EXCLUDED_INDEXES': ['web.search_indexes.LocalCouponIndex']
+    },
+    'mobile_api': {
+        'ENGINE': 'web.utils.CustomSolrEngine',
+        'URL': 'http://127.0.0.1:8080/solr/mobile_api',
+        'EXCLUDED_INDEXES': ['web.search_indexes.CouponIndex', 'web.search_indexes.MerchantIndex']
     },
 }
