837a516b21d6805cc50b7606e0bef0aa8d491185
Alexander Gaevsky <sasha@sasha0.ru>
2014-02-25 23:58:43 +0200

    reverted temporary changes in settings

--- coupons/prodsettings.py
+++ coupons/prodsettings.py
@@ -58,25 +58,12 @@ LOGGING = {
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'web.utils.CustomSolrEngine',
-        'URL': 'http://192.241.162.96:8080/solr',
+        'URL': 'http://127.0.0.1:8080/solr',
         'EXCLUDED_INDEXES': ['web.search_indexes.LocalCouponIndex', 'web.search_indexes.CityPictureIndex']
     },
     'mobile_api': {
         'ENGINE': 'web.utils.CustomSolrEngine',
-        'URL': 'http://192.241.162.96:8080/solr/mobile_api',
+        'URL': 'http://127.0.0.1:8080/solr/mobile_api',
         'EXCLUDED_INDEXES': ['web.search_indexes.CouponIndex', 'web.search_indexes.MerchantIndex']
     },
 }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.mysql',
-        'NAME': 'coupons',
-        'USER': 'pushpenny_mobile',
-        'PASSWORD': 'bmp@V73_q^UT[F>^Z?,vj2zK',
-        'HOST': '192.241.162.96',
-        'DEFAULT_STORAGE_ENGINE': 'MyISAM',
-        'OPTIONS': { 'init_command': 'SET storage_engine=MYISAM' },
-    }
-}
-
