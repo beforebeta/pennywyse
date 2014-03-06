08241e69c80f85d097b9418f3984d399974957be
Alexander Gaevsky <sasha@sasha0.ru>
2014-03-06 03:05:03 +0200

    replaced sessions to redis

--- coupons/basesettings.py
+++ coupons/basesettings.py
@@ -5,6 +5,8 @@ djcelery.setup_loader()
 
 __author__ = 'amrish'
 
+SESSION_ENGINE = 'redis_sessions.session'
+
 FMTC_ACCESS_KEY = '43a787c3f5f2cf2f675cbf86aff6a33b'
 SQOOT_PUBLIC_KEY = 'h7n8we'
 
@@ -178,35 +180,6 @@ INSTALLED_APPS = (
     'websvcs',
     'tastypie',
     )
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-            },
-    },
-}
 
 CACHES = {
     'default': {
