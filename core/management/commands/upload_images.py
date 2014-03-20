f3c01708bb8061ad930edad5a5375bebe3bdecc4
Alexander Gaevsky <sasha@sasha0.ru>
2014-03-20 19:58:19 +0200

    fixed typo

--- core/management/commands/upload_images.py
+++ core/management/commands/upload_images.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from core.util import upload_images_to_s3()
+from core.util import upload_images_to_s3
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
